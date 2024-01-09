458d40b8e1c81ecde3ffb9a2f40b097707fc641e
ivanmatviichuk24 <ivanmatviichuk24@gmail.com>
2024-01-09 15:42:51 +0200

    convert users app into microservice

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { UsersModule } from './users.module'
+import { Transport, MicroserviceOptions } from '@nestjs/microservices'
 
 async function bootstrap() {
-  const app = await NestFactory.create(UsersModule)
-  await app.listen(3000)
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    UsersModule,
+    {
+      transport: Transport.MQTT,
+      options: {
+        url: 'mqtt://localhost:1883',
+      },
+    },
+  )
+  await app.listen()
 }
 bootstrap()
--- apps/users/src/users.controller.ts
+++ apps/users/src/users.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller } from '@nestjs/common'
+import { MessagePattern } from '@nestjs/microservices'
 import { UsersService } from './users.service'
 
 @Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get()
+  @MessagePattern('getHello')
   getHello(): string {
     return this.usersService.getHello()
   }
