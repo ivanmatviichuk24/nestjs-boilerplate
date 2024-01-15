53c796a211f5329e5cd9b7398cb112e39a68d6eb
ivanmatviichuk24 <ivanmatviichuk24@gmail.com>
2024-01-15 16:52:43 +0200

    add swagger

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs boilerplate')
+    .setDescription('API description')
+    .setVersion('0.0.1')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(4001)
 }
 bootstrap()
